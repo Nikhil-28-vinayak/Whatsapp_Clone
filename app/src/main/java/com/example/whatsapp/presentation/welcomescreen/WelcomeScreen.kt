f089455b97206e51a40246f63456545e198e8582
Nikhil-28-vinayak <vinayaknikhil2224@gmail.com>
2025-11-04 22:06:29 +0530

    I completed the formation of welcome screen in whatsapp app

--- app/src/main/java/com/example/whatsapp/presentation/welcomescreen/WelcomeScreen.kt
+++ app/src/main/java/com/example/whatsapp/presentation/welcomescreen/WelcomeScreen.kt
@@ -3,20 +3,64 @@ package com.example.whatsapp.presentation.welcomescreen
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.whatsapp.R
 
 @Composable
 @Preview(showBackground = true)
 fun WelcomeScreen() {
-    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
-        Image(painterResource(R.drawable.whatsapp_sticker), contentDescription = null, modifier = Modifier.size(300.dp) )
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painterResource(R.drawable.whatsapp_sticker),
+            contentDescription = null,
+            modifier = Modifier.size(300.dp)
+        )
+        Text("Welcome to Whatsapp", fontSize = 24.sp, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(24.dp))
+        Row() {
+            Text("Read our", color = Color.Gray)
+            Spacer(modifier = Modifier.width(8.dp))
+            Text("Privacy Policy", color = colorResource(R.color.light_green))
+            Spacer(modifier = Modifier.width(8.dp))
+            Text("Tap Agree and continue to", color = Color.Gray)
+        }
+        Row() {
+            Text("accept the", color = Color.Gray)
+            Spacer(modifier = Modifier.width(8.dp))
+            Text("Terms of Services", color = colorResource(R.color.light_green))
+        }
+        Spacer(modifier = Modifier.height(24.dp))
+        Button(
+            onClick = {},
+            modifier = Modifier.size(240.dp, 43.dp),
+            shape = RoundedCornerShape(8.dp),
+            colors = ButtonDefaults.buttonColors(colorResource(R.color.dark_green))
+        ) {
+            Text("Agree and Continue", fontSize = 16.sp)
+        }
     }
+
 }
