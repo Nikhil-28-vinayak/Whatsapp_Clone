afe1faeb67dec500cd8b76eb5858b55a9175e727
Nikhil-28-vinayak <vinayaknikhil2224@gmail.com>
2025-11-05 11:01:28 +0530

    Now I am Start building user registration screen of whatsapp

---
+++ app/src/main/java/com/example/whatsapp/presentation/userregistrationscreen/UserRegistrationScreen.kt
@@ -0,0 +1,15 @@
+package com.example.whatsapp.presentation.userregistrationscreen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview(showBackground = true)
+fun UserRegistrationScreen(){
+    Column {
+        Text("Enter your phone number")
+
+     }
+}
