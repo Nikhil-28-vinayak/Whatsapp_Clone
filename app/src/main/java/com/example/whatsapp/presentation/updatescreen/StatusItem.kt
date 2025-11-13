1355d91d483ae356152bc40112a5f9ea2c5c8fc4
Nikhil-28-vinayak <vinayaknikhil2224@gmail.com>
2025-11-13 20:06:15 +0530

    Now execution of the status is completed

--- app/src/main/java/com/example/whatsapp/presentation/updatescreen/StatusItem.kt
+++ app/src/main/java/com/example/whatsapp/presentation/updatescreen/StatusItem.kt
@@ -1,2 +1,99 @@
 package com.example.whatsapp.presentation.updatescreen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.whatsapp.R
+
+@Composable
+fun MyStatus() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(12.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box() {
+            Image(
+                painter = painterResource(R.drawable.bhuvan_bam),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(60.dp)
+                    .clip(CircleShape),
+                contentScale = ContentScale.Crop
+            )
+            Icon(
+                painterResource(R.drawable.baseline_add),
+                contentDescription = null,
+                tint = Color.White,
+                modifier = Modifier
+                    .align(
+                        Alignment.BottomEnd
+                    )
+                    .padding(2.dp)
+                    .background(
+                        colorResource(R.color.light_green),
+                        shape = RoundedCornerShape(12.dp)
+                    )
+            )
+        }
+        Spacer(modifier = Modifier.width(12.dp))
+        Column {
+            Text("My Status", fontWeight = FontWeight.Bold, fontSize = 16.sp)
+            Text("Tap to add status update", color = Color.Gray, fontSize = 14.sp)
+        }
+    }
+}
+
+data class Statusdata(
+    val image: Int,
+    val name: String,
+    val time: String
+)
+
+@Composable
+fun StatusItem(statusdata: Statusdata) {
+    Row(
+        modifier = Modifier
+            .padding(horizontal = 12.dp, vertical = 8.dp)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painterResource(statusdata.image),
+            contentDescription = null,
+            modifier = Modifier
+                .size(60.dp)
+                .clip(CircleShape),
+            contentScale = ContentScale.Crop
+        )
+        Spacer(modifier = Modifier.width(12.dp))
+        Column {
+            Text(statusdata.name, fontSize = 16.sp, fontWeight = FontWeight.Bold)
+            Text(statusdata.time, fontSize = 14.sp, color = Color.Gray)
+        }
+    }
+}
--- app/src/main/java/com/example/whatsapp/presentation/updatescreen/UpdateScreen.kt
+++ app/src/main/java/com/example/whatsapp/presentation/updatescreen/UpdateScreen.kt
@@ -1,24 +1,56 @@
 package com.example.whatsapp.presentation.updatescreen
 
+import androidx.compose.foundation.gestures.ScrollableState
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.whatsapp.R
 import com.example.whatsapp.presentation.bottomnavigation.BottomNavigation
 
 @Composable
 @Preview(showBackground = true, showSystemUi = true)
 fun UpdateScreen() {
+    val scrollState = rememberScrollState()
+    var statusList = listOf<Statusdata>(
+        Statusdata(
+            image = R.drawable.boy,
+            name = "Salman Khan",
+            time = "5:00 PM"
+        ),
+        Statusdata(
+            image = R.drawable.girl,
+            name = "Salman Khan",
+            time = "5:00 PM"
+        ),
+        Statusdata(
+            image = R.drawable.boy3,
+            name = "Salman Khan",
+            time = "5:00 PM"
+        ),
+        Statusdata(
+            image = R.drawable.girl2,
+            name = "Salman Khan",
+            time = "5:00 PM"
+        )
+
+    )
     Scaffold(floatingActionButton = {
         FloatingActionButton(
             onClick = {},
@@ -33,7 +65,24 @@ fun UpdateScreen() {
     }, topBar = {
         TopAppBar()
     }) {
-        Column(modifier = Modifier.padding(it)) {
+        Column(
+            modifier = Modifier
+                .padding(it)
+                .fillMaxSize()
+                .verticalScroll(scrollState)
+        ) {
+            Text(
+                "Status",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Black,
+                modifier = Modifier.padding(horizontal = 12.dp, vertical = 8.dp)
+            )
+            MyStatus()
+            statusList.forEach {
+                StatusItem(statusdata = it)
+
+            }
 
         }
     }
