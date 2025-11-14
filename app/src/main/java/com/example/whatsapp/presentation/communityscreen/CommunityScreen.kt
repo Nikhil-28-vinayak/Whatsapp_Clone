d239dbe9bf0edd9d74c4848a133a288efaad5eee
Nikhil-28-vinayak <vinayaknikhil2224@gmail.com>
2025-11-14 20:23:02 +0530

    Now I start executing the community screen . In community screen i added top app bar and bottom navigation

---
+++ app/src/main/java/com/example/whatsapp/presentation/communityscreen/CommunityScreen.kt
@@ -0,0 +1,129 @@
+package com.example.whatsapp.presentation.communityscreen
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.whatsapp.R
+import com.example.whatsapp.presentation.bottomnavigation.BottomNavigation
+
+
+@Composable
+@Preview(showBackground = true, showSystemUi = true)
+fun CommunityScreen() {
+    var isSearching by remember { mutableStateOf(false) }
+    var search by remember { mutableStateOf("") }
+    var showMenu by remember { mutableStateOf(false) }
+
+    Scaffold(
+        topBar = {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(WindowInsets.statusBars.asPaddingValues())
+            ) {
+                Column {
+                    Row {
+                        if (isSearching) {
+                            TextField(
+                                value = search,
+                                onValueChange = { search = it },
+                                placeholder = { Text("Search") },
+                                colors = TextFieldDefaults.colors(
+                                    unfocusedContainerColor = Color.Transparent,
+                                    focusedContainerColor = Color.Transparent,
+                                    unfocusedIndicatorColor = Color.Transparent,
+                                    focusedIndicatorColor = Color.Transparent
+                                ),
+                                singleLine = true,
+                                modifier = Modifier.padding(start = 12.dp)
+                            )
+                        } else {
+                            Text(
+                                "Communities",
+                                fontSize = 28.sp,
+                                color = Color.Black,
+                                modifier = Modifier.padding(start = 12.dp, top = 4.dp)
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.weight(1f))
+
+                        if (isSearching) {
+                            IconButton(onClick = {
+                                isSearching = false
+                                search = ""
+                            }) {
+                                Icon(
+                                    painter = painterResource(R.drawable.cross),
+                                    contentDescription = null,
+                                    modifier = Modifier.padding(15.dp)
+                                )
+                            }
+                        } else {
+                            IconButton(onClick = {
+                                isSearching = true
+                            }) {
+                                Icon(
+                                    painter = painterResource(R.drawable.search),
+                                    contentDescription = null,
+                                    modifier = Modifier.size(24.dp)
+                                )
+                            }
+                            IconButton(onClick = { showMenu = true }) {
+                                Icon(
+                                    painter = painterResource(R.drawable.more),
+                                    contentDescription = null,
+                                    modifier = Modifier.size(24.dp)
+                                )
+                                DropdownMenu(
+                                    expanded = showMenu,
+                                    onDismissRequest = { showMenu = false }
+                                ) {
+                                    DropdownMenuItem(
+                                        text = { Text("Setting") },
+                                        onClick = { showMenu = false }
+                                    )
+                                }
+                            }
+                        }
+                    }
+                    HorizontalDivider()
+                }
+            }
+        }, bottomBar = {
+            BottomNavigation()
+        }
+    ) { paddingValues ->
+        Column(modifier = Modifier.padding(paddingValues)) {
+            // Your screen content goes here
+        }
+    }
+}
