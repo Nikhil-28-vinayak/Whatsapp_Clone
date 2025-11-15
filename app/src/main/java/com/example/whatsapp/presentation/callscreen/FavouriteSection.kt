f63b77124241f2c93a99c279008d919f8c201520
Nikhil-28-vinayak <vinayaknikhil2224@gmail.com>
2025-11-15 13:05:28 +0530

    now start executing the call screen of the whatsapp and in the cal, screen top app is done and now I start executing the Favourite section in the call screen in whatsapp

---
+++ app/src/main/java/com/example/whatsapp/presentation/callscreen/FavouriteSection.kt
@@ -0,0 +1,10 @@
+package com.example.whatsapp.presentation.callscreen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview(showBackground = true, showSystemUi = true)
+fun FavouriteSection(){
+
+}
